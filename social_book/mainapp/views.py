fbe7be3fa2d35154a4986ac5c2218c23deaaaa58
Saurabh Panchal <140837497+saurabhpanchaly23@users.noreply.github.com>
2024-03-13 17:55:45 +0530

    csac

--- social_book/mainapp/views.py
+++ social_book/mainapp/views.py
@@ -59,7 +59,7 @@ def register(request):
             return redirect('login')  # Redirect to login page
     else:
         form = CustomUserCreationForm()
-    return render(request, 'registration.html'{'form': form})
+    return render(request, 'register.html', {'form': form})
 
 @login_required(    login_url='login')
 def index(request):
